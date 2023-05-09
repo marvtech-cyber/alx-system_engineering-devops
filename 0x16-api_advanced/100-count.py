1cf2301d4f04cae23b62c54ae128fe631b496b59
marvtech-cyber <marvin.nyarenchi@gmail.com>
2023-05-09 06:04:00 -0700

    advance fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,42 +1,49 @@
-#!/usr/bin/python3
-"""Contains the count_words function"""
-import requests
+s module defines the number_of_subscribers function.
+"""
 
+import praw
 
-def count_words(subreddit, word_list, found_list=[], after=None):
-    '''Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        found_list (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-    '''
-    user_agent = {'User-agent': 'test45'}
-    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                         .format(subreddit, after), headers=user_agent)
-    if after is None:
-        word_list = [word.lower() for word in word_list]
+def count_words(subreddit, word_list, hot_list=None, count_dict=None):
+    # Create PRAW instance
+    reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
+                         client_secret='YOUR_CLIENT_SECRET',
+                         user_agent='YOUR_USER_AGENT')
 
-    if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in title.split(' '):
-                if word in word_list:
-                    found_list.append(word)
-        if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
-        else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
-                print('{}: {}'.format(key, value))
+    # Create hot_list and count_dict if not provided
+    if hot_list is None:
+        hot_list = []
+    if count_dict is None:
+        count_dict = {}
+
+    # Get hot articles from subreddit
+    subreddit_obj = reddit.subreddit(subreddit)
+    hot_articles = subreddit_obj.hot(limit=100)
+
+    # Iterate through hot articles and add titles to hot_list
+    for article in hot_articles:
+        hot_list.append(article.title)
+
+    # Count occurrences of each word in hot_list
+    for word in word_list:
+        count = sum(1 for title in hot_list if word.lower() in title.lower().split())
+        if count > 0:
+            # Add word to count_dict
+            if word.lower() not in count_dict:
+                count_dict[word.lower()] = count
+            else:
+                count_dict[word.lower()] += count
+
+    # If there are more hot articles, recursively call function with updated hot_list and count_dict
+    if subreddit_obj._path.endswith('/hot'):
+        next_subreddit = subreddit_obj._path[:-4]
     else:
-        return
+        next_subreddit = subreddit_obj._path + '/hot'
+    try:
+        return count_words(next_subreddit, word_list, hot_list=hot_list, count_dict=count_dict)
+    except:
+        pass
+
+    # Sort count_dict by count and then alphabetically
+    sorted_count = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_count:
+        print(word + ':', count)
